022d473ca567bc7b288f635b88130eaf55d04b63
Gustavo <gusalv2001@gmail.com>
2022-07-04 22:50:11 -0400

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAe3TG-GSBEd9K4H4jXpWJaOdlaWyONT1A",
+  authDomain: "first-fb-61007.firebaseapp.com",
+  projectId: "first-fb-61007",
+  storageBucket: "first-fb-61007.appspot.com",
+  messagingSenderId: "276839993341",
+  appId: "1:276839993341:web:469d9d66a5994d7b201394",
+  measurementId: "G-JVWF2VRN8Z",
+};
+
+const app = initializeApp(firebaseConfig);
